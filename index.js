219e43a40758b6cc47c104b56de32a679f891145
backdevhong <ghddls0704@kakao.com>
2023-10-02 00:38:46 +0900

    edit

--- index.js
+++ index.js
@@ -24,13 +24,26 @@ client.on(Events.ClientReady, async (client) => {
         return;
       }
       if (
-        member.roles.cache.some((role) => role.id === "1144269909405225021") ||
+        member.roles.cache.some((role) => role.id === "1144269909405225021")
+      ) {
+        if (
+          member.roles.cache.some((role) => role.id === "980761785147748373")
+        ) {
+          member.roles.remove("980761785147748373");
+          client.channels.cache.get("1143484641718837318").send({
+            message: `<@${member.user.id}> 님이 미자 판정을 받았습니다.`,
+          });
+        }
+      } else if (
         member.roles.cache.some((role) => role.id === "1140989896220233920")
       ) {
         if (
           member.roles.cache.some((role) => role.id === "980761785147748373")
         ) {
           member.roles.remove("980761785147748373");
+          client.channels.cache.get("1141779502704361624").send({
+            message: `<@${member.user.id}> 님이 바보 판정을 받았습니다.`,
+          });
         }
       }
     });
