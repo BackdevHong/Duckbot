29a839487325a222e04fe18bb46031b2945469f2
backdevhong <ghddls0704@kakao.com>
2023-10-02 01:26:15 +0900

    thread end

--- index.js
+++ index.js
@@ -11,15 +11,32 @@ dotenv.config();
 const client = new Client({
   intents: [GatewayIntentBits.GuildMembers, GatewayIntentBits.Guilds],
 });
+client.on(Events.ThreadUpdate, async (oldThread, newThread) => {
+  if (oldThread.locked == newThread.locked) {
+    return;
+  }
 
+  const guild = client.guilds.cache.get(process.env.GUILD_ID);
+
+  if (!oldThread.locked && newThread.locked) {
+    let res = guild.members.cache.filter(
+      (member) => member.id === newThread.ownerId
+    );
+
+    res.forEach((member) => {
+      member.roles.remove("980761785147748373");
+      member.roles.add("1140989896220233920");
+      client.channels.cache.get("1141779502704361624").send({
+        content: `<@${member.user.id}> 님이 바보 판정을 받았습니다. 사유 : <#${newThread.id}>`,
+      });
+    });
+  }
+});
 client.on(Events.ClientReady, async (client) => {
   console.log("Client ready");
   const guild = client.guilds.cache.get(process.env.GUILD_ID);
 
   let res = await guild.members.fetch();
-  const threads = guild.channels.cache
-    .filter((x) => x.id === "1052857740231462994")
-    .filter((x) => x.isThread);
 
   setInterval(() => {
     res.forEach((member) => {
@@ -46,24 +63,6 @@ client.on(Events.ClientReady, async (client) => {
           member.roles.remove("980761785147748373");
         }
       }
-
-      threads.forEach((thread) => {
-        if (thread.locked === true) {
-          if (thread.ownerId === member.id) {
-            if (
-              member.roles.cache.some(
-                (role) => role.id === "980761785147748373"
-              )
-            ) {
-              member.roles.remove("980761785147748373");
-              member.roles.add("1140989896220233920");
-              client.channels.cache.get("1141779502704361624").send({
-                content: `<@${member.user.id}> 님이 바보 판정을 받았습니다. 사유 : <#${thread.id}>`,
-              });
-            }
-          }
-        }
-      });
     });
   }, 1000);
 });
