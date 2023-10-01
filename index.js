f5dfeba5468923a57404713cfc3fd9547bd9270d
backdevhong <ghddls0704@kakao.com>
2023-10-02 00:56:02 +0900

    thread

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ client.on(Events.ClientReady, async (client) => {
   const guild = client.guilds.cache.get(process.env.GUILD_ID);
 
   let res = await guild.members.fetch();
+  const threads = guild.channels.cache
+    .filter((x) => x.id === "1052857740231462994")
+    .filter((x) => x.isThread);
 
   setInterval(() => {
     res.forEach((member) => {
@@ -41,11 +44,26 @@ client.on(Events.ClientReady, async (client) => {
           member.roles.cache.some((role) => role.id === "980761785147748373")
         ) {
           member.roles.remove("980761785147748373");
-          client.channels.cache.get("1141779502704361624").send({
-            content: `<@${member.user.id}> 님이 바보 판정을 받았습니다.`,
-          });
         }
       }
+
+      threads.forEach((thread) => {
+        if (thread.locked === true) {
+          if (thread.ownerId === member.id) {
+            if (
+              member.roles.cache.some(
+                (role) => role.id === "980761785147748373"
+              )
+            ) {
+              member.roles.remove("980761785147748373");
+              member.roles.add("1140989896220233920");
+              client.channels.cache.get("1141779502704361624").send({
+                content: `<@${member.user.id}> 님이 바보 판정을 받았습니다. 사유 : <#${thread.id}>`,
+              });
+            }
+          }
+        }
+      });
     });
   }, 1000);
 });
