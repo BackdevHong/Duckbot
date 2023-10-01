b23b6b138989d68dd32d0e2850f9a938ddaef0bb
backdevhong <ghddls0704@kakao.com>
2023-10-02 01:41:50 +0900

    asdf

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ dotenv.config();
 const client = new Client({
   intents: [GatewayIntentBits.GuildMembers, GatewayIntentBits.Guilds],
 });
+
 client.on(Events.ThreadUpdate, async (oldThread, newThread) => {
   if (oldThread.locked == newThread.locked) {
     return;
@@ -31,39 +32,44 @@ client.on(Events.ThreadUpdate, async (oldThread, newThread) => {
     });
   }
 });
+
+client.on(Events.GuildMemberUpdate, async (oldMember, newMember) => {
+  // 미자
+  if (
+    !oldMember.roles.cache.has("1144269909405225021") &&
+    newMember.roles.cache.has("1144269909405225021")
+  ) {
+    if (newMember.roles.cache.has("980761785147748373")) {
+      newMember.roles.remove("980761785147748373");
+      client.channels.cache.get("1143484641718837318").send({
+        content: `<@${member.user.id}> 님이 미자 판정을 받았습니다.`,
+      });
+    }
+  }
+
+  // 바보
+  if (
+    !oldMember.roles.cache.has("1140989896220233920") &&
+    newMember.roles.cache.has("1140989896220233920")
+  ) {
+    if (newMember.roles.cache.has("980761785147748373")) {
+      newMember.roles.remove("980761785147748373");
+    }
+  }
+
+  // 경고 해제
+  if (
+    oldMember.roles.cache.has("1104721596515627058") &&
+    !newMember.roles.cache.has("1104721596515627058")
+  ) {
+    client.channels.cache.get("1141779502704361624").send({
+      content: `<@${member.user.id}> 님의 경고가 해제되었습니다.`,
+    });
+  }
+});
+
 client.on(Events.ClientReady, async (client) => {
   console.log("Client ready");
-  const guild = client.guilds.cache.get(process.env.GUILD_ID);
-
-  let res = await guild.members.fetch();
-
-  setInterval(() => {
-    res.forEach((member) => {
-      if (member.user.bot == true) {
-        return;
-      }
-      if (
-        member.roles.cache.some((role) => role.id === "1144269909405225021")
-      ) {
-        if (
-          member.roles.cache.some((role) => role.id === "980761785147748373")
-        ) {
-          member.roles.remove("980761785147748373");
-          client.channels.cache.get("1143484641718837318").send({
-            content: `<@${member.user.id}> 님이 미자 판정을 받았습니다.`,
-          });
-        }
-      } else if (
-        member.roles.cache.some((role) => role.id === "1140989896220233920")
-      ) {
-        if (
-          member.roles.cache.some((role) => role.id === "980761785147748373")
-        ) {
-          member.roles.remove("980761785147748373");
-        }
-      }
-    });
-  }, 1000);
 });
 
 client.login(process.env.TOKEN);
